ed7f64f39423b7c648988cfa4896f88c77964771
thesimran123 <simmikashyap2912@gmail.com>
2023-10-07 10:55:07 +0530

    first commit

---
+++ steps/src/components/PackingList.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import Item from "./Item";
+
+export default function PackingList({
+  items,
+  onDeleteItem,
+  onToggleItem,
+  onCLearList,
+}) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  return (
+    <div className="list">
+      <ul>
+        {sortedItems.map((item) => (
+          <Item
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+            key={item.id}
+          />
+        ))}
+      </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by the input order</option>
+          <option value="description">Sort by the description</option>
+          <option value="packed">Sort by the packed</option>
+        </select>
+        <button onClick={onCLearList}>Clear List</button>
+      </div>
+    </div>
+  );
+}
